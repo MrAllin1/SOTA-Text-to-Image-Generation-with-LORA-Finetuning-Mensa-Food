3d0c63eea0effcfe3e981d089388addc7c6563ee
MrAllin1 <alidemandi@gmail.com>
2025-07-10 08:56:18 +0200

    Track .safetensors with Git LFS

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mensa_t2i
 SoTA T2I Adapting and Finetuning
+
+
+chmod +x train_lora_job.sh
+sbatch train_lora_job.sh
+
+
+squeue --me
+sacct -j 19425405 --format=JobID,State,Elapsed,MaxRSS
+19425346
---
+++ data_loader.py
@@ -0,0 +1,45 @@
+# data_loader.py
+
+import os
+from datasets import load_dataset
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+def make_transforms(resolution: int):
+    return transforms.Compose([
+        transforms.Resize(resolution),
+        transforms.CenterCrop(resolution),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5]),
+    ])
+
+def get_dataloader(
+    csv_file: str,
+    tokenizer,
+    resolution: int,
+    batch_size: int,
+    num_workers: int = 4,
+    split: str = "train",
+):
+    ds = load_dataset("csv", data_files={split: csv_file}, split=split)
+    base_dir = os.path.dirname(csv_file)
+    tfm = make_transforms(resolution)
+    max_length = 77
+
+    def preprocess(example):
+        img_path = os.path.join(base_dir, example["image_path"])
+        img = Image.open(img_path).convert("RGB")
+        pixel_values = tfm(img)
+        input_ids = tokenizer(
+            example["description"],
+            truncation=True,
+            padding="max_length",
+            max_length=max_length,
+        ).input_ids
+        return {"pixel_values": pixel_values, "input_ids": input_ids}
+
+    ds = ds.map(preprocess,remove_columns=ds.column_names,num_proc=1,)
+    ds.set_format(type="torch", columns=["pixel_values", "input_ids"])
+    return DataLoader(ds, batch_size=batch_size, shuffle=True, num_workers=num_workers)
